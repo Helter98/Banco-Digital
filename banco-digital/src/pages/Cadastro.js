326458b9b2001eaa3a5cf91b810ff0a3415d0137
Helter Xavier <helter.xavier@hotmail.com>
2022-08-30 14:40:06 -0300

    3008form

--- banco-digital/src/pages/Cadastro.js
+++ banco-digital/src/pages/Cadastro.js
@@ -1,83 +1,136 @@
 import './Cadastro.css';
 import { useForm } from "react-hook-form";
 import { useState } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import InputMask from 'react-input-mask';
 
 
+
+//Tratamento de erros no preenchimento do formuláro
+const schema = yup.object({
+  name: yup.string().required('O nome é obrigatório'),
+  email: yup.string().email('Digite um email válido').required('O email é obrigatório'),
+  password: yup.string().min(6, 'A senha deve ter pelo menos 6 digitos').required('A senha é obrigatório'),
+  confirmPassword: yup.string().required('Confirmar a senha é obrigatório').oneOf([yup.ref("password"), "As senhas deve ser iguais"]),
+}).required();
 
 const Cadastro = () => {
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+// USE FORM
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
 
-  const [showPessoaFisica, setshowPessoaFisica] = useState(false)
-  const showFisica = () => setshowPessoaFisica(true)
-
+  //SELECIONAR A OPÇÃO DE CADASTRO DE PESSOA JURIDICA
   const [showPessoaJuridica, setshowPessoaJuridica] = useState(false)
   const showJuridica = () => setshowPessoaJuridica(true)
 
-
-  const onSubmit = (userData) => {
-    console.log(userData)
+  //Enviar formulário
+  const onSubmit = (registerUser) => {
+    console.log('formulario enviado')
   }
+  
   return (
+//PAGINA DE CADASTRO
     <div className='Cadastro'>
-
       <div className='heroCadastro'>
         <h1>Complete os campos ao lado
           <br />para se cadastrar <br /> em nosso banco</h1>
 
+          {/* FORMULÁRIO DE CADASTRO */}
+          {/* ENVIO DO FORMULÁRIO */}
         <form onSubmit={handleSubmit(onSubmit)}>
+          {/* SELECIONAR A OPÇÃO DE CADASTRO DE PESSOA JURIDICA NO CLICK */}
           <div className="selectForm">
             <label>
-              <input className='buttonSelect' type="button" value="Para você" onClick={showFisica} />
-            </label>
-
-            <label>
-              <input className='buttonSelect' type="button" value="Para sua Empresa" onClick={showJuridica} />
+            {/* */}  <input className='opSelect' type="text" value="Para sua Empresa" onClick={showJuridica} />
             </label>
           </div>
-
-          {showPessoaFisica ?
-            <div className='pessoaFisica'>
-              <label>
-                <input placeholder='CPF' {...register("cpf", { required: true })} />
-              </label>
-
-              <label >
-                <input placeholder='Nome completo' {...register("name", { required: true })} />
-              </label>
-
-              <label>
-                <input placeholder='Email' {...register("email", { required: true })} />
-              </label>
-
-              <label>
-                <input placeholder='Senha' {...register("password", { required: true })} />
-              </label>
-
-              <button className='btnPCadastro' type='submit'>Enviar</button>
-            </div> : null}
-
-
+          
+          {/* CONDICIONAL PARA MOSTRAR FORMULÁRIO PESSOA JURIDICA NO CLICK */}
           {showPessoaJuridica ?
-            <div className='pessoaFisica'>
+            <div className='pessoa'>
               <label>
-                <input placeholder='CNPJ' {...register("cnpj", { required: true })} />
+                <input type="text" placeholder='CNPJ' {...register("cnpj", { required: true })} />
+                {errors.cnpj &&
+                  <span>dfsdafsdf</span>
+                }
               </label>
 
               <label >
-                <input placeholder='Razão Social' {...register("name", { required: true })} />
+                <input type="text" placeholder='Razão Social' {...register("name", { required: true })} />
+                {errors.name &&
+                  <span>{errors.name?.message}</span>
+                }
               </label>
 
               <label>
-                <input placeholder='Email' {...register("email", { required: true })} />
+                <input type="email" placeholder='Email' {...register("email", { required: true })} />
+                {errors.email &&
+                  <span>Este campo é obrigatório</span>
+                }
               </label>
 
               <label>
-                <input placeholder='Senha' {...register("password", { required: true })} />
+                <input type="password" placeholder='Senha' {...register("password", { required: true })} />
+                {errors.password &&
+                  <span>{errors.password?.message}</span>
+                }
+              </label>
+
+              <label>
+                <input type="password" placeholder='Confirmar Senha' {...register("confirmPassword", { required: true })} />
+                {errors.confirmPassword &&
+                  <span>{errors.confirmPassword?.message}</span>
+                }
               </label>
 
               <button className='btnPCadastro' type='submit'>Enviar</button>
-            </div> : null}
+            </div>
+            // AO ABRIR A PÁGINA OU QUANDO NÃO FOR CLICADO EM NADA VALOR NULL ABRE DIRETO NA PAGINA DE PESSOA FISICA
+            : 
+            <div className='pessoa'>
+              <label>
+              <InputMask placeholder='CPF' mask="999.999.999-99" />
+               {/* <input type="text" placeholder='CPF' {...register("cpf", { required: true })} /> */}
+                {errors.cpf &&
+                  <span>Este campo é obrigatório</span>
+                }
+              </label>
+
+              <label >
+                <input type="text" placeholder='Nome completo' {...register("name", { required: true })} />
+                {errors.name &&
+                  <span>{errors.name?.message}</span>
+                }
+              </label>
+
+              <label>
+                <input type="email" placeholder='Email' {...register("email", { required: true })} />
+                {errors.email &&
+                  <span>{errors.email?.message}</span>
+                }
+              </label>
+
+              <label>
+                <input type="password" placeholder='Senha' {...register("password", { required: true })} />
+                {errors.password &&
+                  <span>{errors.password?.message}</span>
+                }
+              </label>
+
+              <label>
+                <input type="password" placeholder='Confirmar Senha' {...register("confirmPassword", { required: true })} />
+                {errors.confirmPassword &&
+                  <span>{errors.confirmPassword?.message}</span>
+                }
+              </label>
+
+             {/* VALIDAÇÃO DO FORMULARIO NO MOMENTO DO CLICK*/}
+              <button className='btnPCadastro' type='submit'>Enviar</button>
+            </div>
+          }
         </form>
       </div>
       <div>
@@ -86,6 +139,4 @@ const Cadastro = () => {
 
   )
 }
-
-
 export default Cadastro
--- banco-digital/src/pages/Home.js
+++ banco-digital/src/pages/Home.js
@@ -8,8 +8,8 @@ const Home = () => {
         <div className="infoCadastro">
           <h1>Conta grátis, 100% digital e <br />com serviços ilimitados
 
-           <span className='spanColor'>para você</span> 
-           <span className='spanColor2'>fazer acontecer </span>
+           <h1 className='spanColor'>para você</h1> 
+           <h1 className='spanColor2'>fazer acontecer </h1>
            </h1>
         </div>
 
