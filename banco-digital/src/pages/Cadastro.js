ecb089c60c5437d53ba6eea6eee736dd2e90bafa
Helter Xavier <helter.xavier@hotmail.com>
2022-08-31 21:36:07 -0300

    at

--- banco-digital/src/pages/Cadastro.js
+++ banco-digital/src/pages/Cadastro.js
@@ -7,52 +7,53 @@ import InputMask from 'react-input-mask';
 const Cadastro = () => {
 
   const { register, handleSubmit, formState: { erros } } = useForm();
-  
 
   const [type, setType] = useState("CPF")
 
+  const addCadastro = data => axios.post(data)
+  .then(() => {
+    console.log('deu certo')
+  })
+  .catch(() => {
+    console.log('erro')
+  })
 
   return (
     <div className='Cadastro'>
       <div className='heroCadastro'>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(addCadastro)}>
           <h1>Cadastro</h1>
-          <div className="allForm">
-            <div className="formCheck">
-              <div className='inputRadioRow'>
+          <div className="formCheck">
+            <div className='inputRadioRow'>
               <div className="inputRadio">
-              <input type="radio" name="document_type" id="CPF" value={"CPF"} onClick={(e) => setType(e.target.value)} />
+                <input type="radio" name="document_type" id="CPF" value={"CPF"} {...register("document_type", { required: true })} onClick={(e) => setType(e.target.value)} />
                 <label>Pessoa Fisica</label>
               </div>
               <div className="inputRadio">
-              <input type="radio" name="document_type" id="CNPJ" value={"CNPJ"} onClick={(e) => setType(e.target.value)} />
+                <input type="radio" name="document_type" id="CNPJ" value={"CNPJ"} {...register("document_type", { required: true })} onClick={(e) => setType(e.target.value)} />
                 <label>Pessoa Juridica</label>
-                </div>
               </div>
             </div>
+          </div>
+          <div className='inputMaskType'>
+            {type === "CPF" ?
+              <InputMask className="styleInfo" mask="999.999.999-99" type="text" name='document_number' {...register("ducument_number")} placeholder="CPF" />
+              : <InputMask className="styleInfo" mask="99.99.999/9999-99" type="text" name='document_number' {...register("ducument_number")} placeholder="CNPJ" />
+            }
+          </div>
 
-                <div className='inputMaskType'>
-                {type === "CPF" ?  
-                  <InputMask className="styleInfo" mask="999.999.999-99" type="text" name='document_number' {...register("ducument_number")}  placeholder="CPF"/> 
-                : <InputMask className="styleInfo" mask="99.99.999/9999-99" type="text" name='document_number' {...register("ducument_number")}  placeholder="CNPJ" /> 
-                }
-                </div>
-            
-                <div className='inputInfo'>
-                  <label htmlFor="name">
-                    <input className='styleInfo' type="text" name="name" id="name" {...register("name")} placeholder="Nome Completo" />
-                  </label>
-                  <label htmlFor="name">
-                    <input className='styleInfo' type="email" name="email" id="email" {...register("email", { required: true })} placeholder='Email' />
-                  </label>
-                  <label htmlFor="">
-                  <input className="styleInfo" name="password" id="password" {...register("password")} type="password" placeholder="Senha" />
-                  </label>
-                </div>
-              </div>
-
-        <input id='btnCadastro' type="submit" value="Enviar" />
-
+          <div className='inputInfo'>
+            <label htmlFor="name">
+              <input className='styleInfo' type="text" name="name" id="name" {...register("name")} placeholder="Nome Completo" />
+            </label>
+            <label htmlFor="name">
+              <input className='styleInfo' type="email" name="email" id="email" {...register("email", { required: true })} placeholder='Email' />
+            </label>
+            <label htmlFor="">
+              <input className="styleInfo" name="password" id="password" {...register("password")} type="password" placeholder="Senha" />
+            </label>
+          </div>
+          <input id='btnCadastro' type="submit" value="Enviar" />
         </form>
       </div>
     </div>
